f4bbf736a3761d4cfad43a53cd8c1c4774cdc7e0
CHRIS GEORGE <Chrisgeorge2k20@gmail.com>
2025-11-19 16:13:28 +0530

    removed default fonts

--- ai-website-generator/app/page.tsx
+++ ai-website-generator/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       hello
+      <Button>click here</Button>
     </div>
   );
 }
