1146e12388bbb27ee0d9f591610b21d2b9e7debb
CHRIS GEORGE <Chrisgeorge2k20@gmail.com>
2025-11-19 19:31:53 +0530

    Added Menu Options

--- ai-website-generator/app/_components/Header.tsx
+++ ai-website-generator/app/_components/Header.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+
+const MenuOptions = [
+  {
+    name: "pricing",
+    path: "/pricing",
+  },
+  {
+    name: "Contact Us",
+    path: "/contact-us",
+  },
+];
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div>
+      {/* Logo */}
+      <div className="flex gap-2 items-center">
+        <Image src={"/logo.svg"} alt="Logo" width={35} height={35} />
+        <h2 className="font-bold text-xl">AI Website Generator</h2>
+      </div>
+
+      {/* Menu Options */}
+      <div>
+        {MenuOptions.map((menu, index) => (
+          <Button key={index}>{menu.name}</Button>
+        ))}
+      </div>
+
+      {/* Get Started Button */}
+    </div>
+  );
 }
 
-export default Header
+export default Header;
