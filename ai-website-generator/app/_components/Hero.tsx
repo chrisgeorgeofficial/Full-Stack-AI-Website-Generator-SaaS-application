99e91f104fe4e38a45c3de65b8c1bbf3fd2caf96
CHRIS GEORGE <Chrisgeorge2k20@gmail.com>
2025-11-20 18:48:49 +0530

    Hero section design started

---
+++ ai-website-generator/app/_components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>
+      {/* Header & Description */}
+      <h2 className='font-bold text-7xl'>What should the design be?</h2>
+      <p className='mt-2 text-xl text-gray-500'>Imagine. Generate. Edit. Explore. Export. </p>
+      {/* input box */}
+      {/* suggestion list */}
+    </div>
+  )
+}
+
+export default Hero
