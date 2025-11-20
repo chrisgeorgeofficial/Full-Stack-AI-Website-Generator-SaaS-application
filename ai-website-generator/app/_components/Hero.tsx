fe0bcf119dce3e7d2c67efff185fa86448a19b74
CHRIS GEORGE <Chrisgeorge2k20@gmail.com>
2025-11-20 21:49:45 +0530

    Update Hero.tsx

--- ai-website-generator/app/_components/Hero.tsx
+++ ai-website-generator/app/_components/Hero.tsx
@@ -1,15 +1,77 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { ImagePlus } from "lucide-react";
+import { ArrowUp } from "lucide-react";
+import { LayoutDashboard, Key, HomeIcon, User } from "lucide-react";
+import { useState } from "react";
+
+const suggestions = [
+  {
+    label: "Dashboard",
+    prompt:
+      "Create an analytics dashboard to track customers and revenue data for a SaaS",
+    icon: LayoutDashboard,
+  },
+  {
+    label: "SignUp Form",
+    prompt:
+      "Create a modern sign up form with email/password fields, Google and Github login options, and terms checkbox",
+    icon: Key,
+  },
+  {
+    label: "Hero",
+    prompt:
+      "Create a modern header and centered hero section for a productivity SaaS. Include a badge for feature announcement, a title with a subtle gradient effect, subtitle, CTA, small social proof and an image.",
+    icon: HomeIcon,
+  },
+  {
+    label: "User Profile Card",
+    prompt:
+      "Create a modern user profile card component for a social media website",
+    icon: User,
+  },
+];
 
 function Hero() {
+
+  const [userInput,setUserInput] = useState<string>();
+
   return (
-    <div>
+    <div className="flex flex-col items-center h-[80vh] justify-center">
       {/* Header & Description */}
-      <h2 className='font-bold text-7xl'>What should the design be?</h2>
-      <p className='mt-2 text-xl text-gray-500'>Imagine. Generate. Edit. Explore. Export. </p>
+      <h2 className="font-bold text-7xl">What should the design be?</h2>
+      <p className="mt-2 text-xl text-gray-500">
+        Imagine. Generate. Edit. Explore. Export.{" "}
+      </p>
       {/* input box */}
+      <div className="w-full max-w-xl p-5 border mt-5 rounded-2xl">
+        <textarea
+          placeholder="Describe your page design"
+          value={userInput}
+          onChange={(event)=>setUserInput(event.target.value)}
+          className="w-full h-24 focus:outline-none focus:ring-0 resize-none"
+        ></textarea>
+        <div className="flex justify-between items-center">
+          <Button variant={"ghost"}>
+            <ImagePlus />
+          </Button>
+          <Button>
+            <ArrowUp />
+          </Button>
+        </div>
+      </div>
       {/* suggestion list */}
+      <div className="mt-4 flex gap-3">
+        {suggestions.map((suggestion, index) => (
+          <Button key={index} variant={'outline'}>
+            <suggestion.icon />
+            {suggestion.label}</Button>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
