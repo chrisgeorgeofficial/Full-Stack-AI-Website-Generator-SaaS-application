624d076078f04fe1aef9588ecb3b92db0ccbb425
CHRIS GEORGE <Chrisgeorge2k20@gmail.com>
2025-11-25 15:26:47 +0530

    Inserting new users into db

--- ai-website-generator/app/api/users/route.ts
+++ ai-website-generator/app/api/users/route.ts
@@ -1,9 +1,24 @@
-import { NextRequest } from "next/server";
+import { db } from "@/config/db";
+import { usersTable } from "@/config/schema";
+import { currentUser } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req:NextRequest) {
+    const user = await currentUser();   
     //if User Already Exits ?
+    const userResult = await db.select().from(usersTable)
+    //@ts-ignore
+    .where(eq(usersTable.email,user?.primaryEmailAddress?.emailAddress));
+
 
     //if Not Then insert user
-
+    if(userResult.length === 0)
+    {
+        const result = await db.insert(usersTable).values({
+            name:user?.fullName??'NA',
+            email:user?.primaryEmailAddress?.emailAddress??'',
+    })
+    }
     return NextResponse.json({})
 }
