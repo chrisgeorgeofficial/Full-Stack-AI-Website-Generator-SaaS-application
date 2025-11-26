41179dfd4cc54bdceca18a0f6b39929853cd1f8a
CHRIS GEORGE <Chrisgeorge2k20@gmail.com>
2025-11-26 13:00:09 +0530

    Update AppSidebar.tsx

--- ai-website-generator/app/workspace/_components/AppSidebar.tsx
+++ ai-website-generator/app/workspace/_components/AppSidebar.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { Button } from "@/components/ui/button";
 import {
   Sidebar,
   SidebarContent,
@@ -6,10 +8,14 @@ import {
   SidebarHeader,
 } from "@/components/ui/sidebar"
 
+import Link from "next/link";
+import { useState } from "react";
+
 //Import Image
 import Image from "next/image";
 
 export function AppSidebar() {
+  const [projectList,setProjectList] = useState([]);
   return (
     <Sidebar>
       <SidebarHeader className="p-5">
@@ -17,9 +23,17 @@ export function AppSidebar() {
             <Image src={'/logo.svg'} alt="logo" width={35} height={35}/>
             <h2 className="font-bold text-xl">AI Website Builder</h2>
         </div>
+        <Link href={'/workspace'} className="mt-5 w-full">
+        <Button className="w-full">
+          + Add New Project
+        </Button>
+        </Link>
       </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
+        <SidebarGroup>
+          {projectList.length ==0&&
+          <h2>No Project Found</h2>}
+        </SidebarGroup>
         <SidebarGroup />
       </SidebarContent>
       <SidebarFooter />
