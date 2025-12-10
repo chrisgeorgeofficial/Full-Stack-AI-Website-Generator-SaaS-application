a21e7cfc38e1e1a90fbd0db2e0499fb8f89e52f2
CHRIS GEORGE <Chrisgeorge2k20@gmail.com>
2025-12-10 12:33:06 +0530

    Create projectTable , chatTable, frameTable

--- ai-website-generator/config/schema.ts
+++ ai-website-generator/config/schema.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, timestamp, varchar, json } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -6,3 +6,25 @@ export const usersTable = pgTable("users", {
   email: varchar({ length: 255 }).notNull().unique(),
   credits:integer().default(2)
 });
+
+
+export const projectTable = pgTable("projects", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  projectId:varchar(),
+  createdBy:varchar().references(() => usersTable.email),
+  createdOn:timestamp().defaultNow()
+})
+
+export const frameTable = pgTable('frames', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  frameId: varchar(),
+  projectId: varchar().references(() => projectTable.projectId),
+  createdOn: timestamp().defaultNow()
+})
+
+export const chatTable=pgTable('chats',{
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  chatMessage: json(),
+  createdBy:varchar().references(() => usersTable.email),
+  createdOn:timestamp().defaultNow()
+})
